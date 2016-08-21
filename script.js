8573c9d31a7f7e4fcf88f385baff3f60ab4e236d
Corentin Pinato <corentin_pinato@hotmail.com>
2016-08-21 14:15:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
    
-    var images = ['normLeft.png','breathLeft.png','walkLeft1.png','walkLeft2.png','normRight.png','breathRight.png','walkRight1.png','walkRight2.png','normUp.png','breatUp.png','walkUp1.png','walkUp2.png','normDown.png','breathDown.png','walkDown1.png','walkDown2.png'];
+    /**
+    * The code needs to be improved.
+    */
+    
+    //Game music looping for ever.
+    var gameTune = new Audio('sound/gameTune.mp3');
+    gameTune.addEventListener('ended',function(){
+        gameTune.play();
+    }, false);
+    gameTune.play();
+    
+    //images representing the character.
+    var images = ['normLeft.png','breathLeft.png','walkLeft1.png','walkLeft2.png','normRight.png','breathRight.png','walkRight1.png','walkRight2.png','normUp.png','breathUp.png','walkUp1.png','walkUp2.png','normDown.png','breathDown.png','walkDown1.png','walkDown2.png'];
+    //index pointing to the current image being displayed.
     var index = 0;
     
+    //interval for the character's breathing when at rest.
+    var breathInterval = setInterval(breath, 500);
+    
+    //Breating interval at work and character's movements.
+    var keydown = false;
     $(document).keydown(function(key){
+        
+        clearInterval(breathInterval);
 
+        gameInput(key);
+
+        breathInterval = setInterval(breath, 500);
+
+
+    });
+    
+    //function taking the game's input and making movements.
+    function gameInput(key){
+        
         switch(parseInt(key.which,10)){
                 
             //Right Arrow pressed go right
@@ -18,8 +48,7 @@ $(document).ready(function(){
                 else{
                     index = 6;
                 }
-                $('#character').attr('src','./character/'+images[index]);
-                $('#character').animate({left: '+=10'}, 0);
+                $('#character').attr('src','./character/'+images[index]).animate({left: '+=10'}, 0);
                 break;
             //Left Arrow pressed go left
             case 37:
@@ -32,8 +61,7 @@ $(document).ready(function(){
                 else{
                     index = 2;
                 }
-                $('#character').attr('src','./character/'+images[index]);
-                $('#character').animate({left: '-=10'}, 0);
+                $('#character').attr('src','./character/'+images[index]).animate({left: '-=10'}, 0);
                 break;
             //Up Arrow pressed go up
             case 38:
@@ -46,8 +74,7 @@ $(document).ready(function(){
                 else{
                     index = 10;
                 }
-                $('#character').attr('src','./character/'+images[index]);
-                $('#character').animate({top: '-=10'}, 0);
+                $('#character').attr('src','./character/'+images[index]).animate({top: '-=10'}, 0);
                 break;
             //Down Arrow pressed go down
             case 40:
@@ -60,11 +87,31 @@ $(document).ready(function(){
                 else{
                     index = 14;
                 }
-                $('#character').attr('src','./character/'+images[index]);
-                $('#character').animate({top: '+=10'}, 10);
+                $('#character').attr('src','./character/'+images[index]).animate({top: '+=10'}, 0);
                 break;
         }
+    }
+    
+    function breath(){
         
-
-    })
+        switch(index){
+            case 0: index = 1; break;
+            case 1: index = 0; break;
+            case 4: index = 5; break;
+            case 5: index = 4; break;
+            case 8: index = 9; break;
+            case 9: index = 8; break;
+            case 12: index = 13; break;
+            case 13: index = 12; break;
+            case 2: index = 0; break;
+            case 3: index = 0; break;
+            case 6: index = 4; break;
+            case 7: index = 4; break;
+            case 10: index = 8; break;
+            case 11: index = 8; break;
+            case 14: index = 12; break;
+            case 15: index = 12; break;
+        }
+        $('#character').attr('src','./character/'+images[index]);
+    }
 });
